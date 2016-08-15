90820d9fb99aa8fa4edc58894f5b7abfcb794e9d
John Jones <john.jones@williamhill.com>
2016-08-15 11:08:27 +0200

    Add the loading of the web views, and also fix constraints on the web view to ensure its fills the page.

--- BMWTrackDays/ViewController.swift
+++ BMWTrackDays/ViewController.swift
@@ -10,16 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let url = NSURL(string: "http://bmwtrackdays.com/forum");
+        let requestObj = NSURLRequest(URL: url!);
+        self.webView.loadRequest(requestObj)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
