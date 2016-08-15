47c7944a425bf308e7f06bbc95b1774c2f109500
John Jones <john.jones@williamhill.com>
2016-08-15 10:47:30 +0200

    Initial Commit

---
+++ BMWTrackDays/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMWTrackDays
+//
+//  Created by John Jones on 15/08/2016.
+//  Copyright © 2016 John Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
