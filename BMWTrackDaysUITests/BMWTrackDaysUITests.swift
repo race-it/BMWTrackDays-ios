67833fad0fa4098e326562522673159c58ffe71a
John Jones <john.jones@williamhill.com>
2017-11-22 11:00:22 +0100

    Adding the certificate.

--- BMWTrackDaysUITests/BMWTrackDaysUITests.swift
+++ BMWTrackDaysUITests/BMWTrackDaysUITests.swift
@@ -9,7 +9,9 @@
 import XCTest
 
 class BMWTrackDaysUITests: XCTestCase {
-        
+    
+    let app = XCUIApplication()
+    
     override func setUp() {
         super.setUp()
         
@@ -18,7 +20,7 @@ class BMWTrackDaysUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        app.launch()
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -31,6 +33,8 @@ class BMWTrackDaysUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        app.links["General"].tap()
+        XCTAssert(app.links["Trackdays"].exists)
     }
     
 }
